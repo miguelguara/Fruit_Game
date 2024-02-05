1e16d3dd03633a798ac739a5cd832e335ded8034
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2024-02-05 01:48:14 -0300

    Fixing some bugs

--- Fruit Game/Assets/Scripts/Destroy.cs
+++ Fruit Game/Assets/Scripts/Destroy.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
+    [SerializeField] string Tags;
+
     private void OnTriggerEnter2D(Collider2D c)
     {
+        if(c.tag != Tags)
         Destroy(c.gameObject);
     }
 }
--- Fruit Game/Assets/Scripts/Gravity.cs
+++ Fruit Game/Assets/Scripts/Gravity.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    public GameObject obj;
+    public GameObject obj_boton,obj_cleanf,obj;
+    
 
     private void OnTriggerEnter2D(Collider2D c)
     {
@@ -14,5 +15,17 @@ public class Gravity : MonoBehaviour
     public void Desativar()
     {
         obj.SetActive(false);
+        obj_boton.SetActive(true);
+    }
+
+    public void Clean_Boton()
+    {
+        StartCoroutine(clean());
+    }
+
+    IEnumerator clean()
+    {   obj_cleanf.SetActive(true);
+        yield return new WaitForSeconds(0.01f);
+        obj_cleanf.SetActive(false);
     }
 }
